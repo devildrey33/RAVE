96cfed96e706820da431bf8cd624cb2b549e81c7
devildrey33 <devildrey33@hotmail.com>
2018-01-07 01:28:45 +0100

    RAVE 0.311b
    ...

--- Reproductor de Audio y Video Extendido/RAVE_Configuracion.h
+++ Reproductor de Audio y Video Extendido/RAVE_Configuracion.h
@@ -1,8 +1,8 @@
 // Configuración para compilar RAVE
 
 // Versión y titulo
-#define RAVE_VERSION			0.309f
-#define RAVE_TITULO				L"Rave 0.309"
+#define RAVE_VERSION			0.311f
+#define RAVE_TITULO				L"Rave 0.311"
 
 // Mostrar consola de depuración
 #define MOSTRAR_CONSOLA
